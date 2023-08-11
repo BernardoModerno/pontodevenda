d044de2b7e063b11f5f02d9f6a2e6adcc2a8b71d
BernardoModerno <bernardo.moderno@gmail.com>
2023-08-11 19:13:59 -0300

    Mostrando Items na tela

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,10 +1,43 @@
-import React from "react";
-import DefaultLayout from "./../components/DefaultLayout";
+import React, {
+  useEffect,
+  useState,
+} from 'react';
+
+import {
+  Col,
+  Row,
+} from 'antd';
+import axios from 'axios';
+
+import DefaultLayout from '../components/DefaultLayout';
+import ItemList from '../components/ItemList';
 
 const Homepage = () => {
+
+  const [itemsData, setItemsData] = useState([]);
+
+  //useEffect
+  useEffect(() => {
+    const getAllItems = async () => {
+      try {
+        const { data } = await axios.get("/api/items/get-item");
+        setItemsData(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAllItems();
+  }, []);
   return (
     <DefaultLayout>
-      <h1>Home Page</h1>
+      <Row>
+        {itemsData.map((item) => (
+          <Col xs={24} lg={6} md={12} sm={6}>
+            <ItemList item={item} />
+          </Col>
+        ))}
+      </Row>
     </DefaultLayout>
   );
 };
